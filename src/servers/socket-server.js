6ba6894bb5192db8add54c8c745f4850a7f26e77
Kevin <kk2229.yang@gmail.com>
2025-12-11 14:36:38 +0800

    refactor: Decouple API and Socket HTTP server instances

--- src/servers/socket-server.js
+++ src/servers/socket-server.js
@@ -1,33 +1,34 @@
-import express from "express";
-import http from "http";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import { Server } from "socket.io";
-import { initializeSocketHandlers } from "../services/socket-service.js";
-import { SOCKET_SERVER_PORT } from "../configs/constants.js";
+import express from 'express'
+import http from 'http'
+import { dirname, join } from 'path'
+import { Server } from 'socket.io'
+import { fileURLToPath } from 'url'
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+import { SOCKET_SERVER_PORT } from '../configs/constants.js'
+import { initializeSocketHandlers } from '../services/socket-service.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
-console.log(__dirname);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+console.log(__dirname)
 
 // Socket 服務專用的靜態文件
-app.use(express.static("socket-public"));
+app.use(express.static('socket-public'))
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "socket-public", "index.html"));
-});
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'socket-public', 'index.html'))
+})
 
 // 初始化 Socket.IO 事件處理
-initializeSocketHandlers(io);
+initializeSocketHandlers(io)
 
-const PORT = SOCKET_SERVER_PORT;
+const PORT = SOCKET_SERVER_PORT
 server.listen(PORT, () => {
-  console.log(`Socket server is running on port ${PORT}`);
-});
+  console.log(`Socket server is running on port ${PORT}`)
+})
 
-export default io;
+export default io
